9ed0999b5c761cd14edb785244f7052d5dfc1cb8
Osita <101451314+oceeta@users.noreply.github.com>
2024-09-17 16:01:43 +0100

    Add condition to test if numberOfReps < 0
    - Pass 5/7 unit tests;

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,9 @@
 const repeatString = function(originalString, numberOfReps) {
     let repeatedString = '';
     let loopCounter = 1;
+    if (numberOfReps < 0){
+        repeatedString = 'ERROR';
+    }
     while (loopCounter <= numberOfReps) {
         repeatedString += originalString;
     loopCounter++;
