58d78f0f187d1d08db87ea23089dc487ab2ed831
Osita <101451314+oceeta@users.noreply.github.com>
2024-09-17 17:04:01 +0100

    Add function to remove one element from an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, itemToRemove) {
+    let newArray = [];
+    for (const element of array) {
+        if (element === itemToRemove){
+            continue;
+        }
+        newArray.push(element)
+    }
+    return newArray;
 };
 
 // Do not edit below this line
