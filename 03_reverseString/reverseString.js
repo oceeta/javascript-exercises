a989de432d04103fb5395b9832ac8c85f99721c1
Osita <101451314+oceeta@users.noreply.github.com>
2024-09-17 16:26:20 +0100

    Write function to reverse a given string
    - Pass 4/4 unit tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(originalString) {
+    let repeatedString = "";
+    for (let i = originalString.length - 1; i >= 0 ; i--) {
+        repeatedString += originalString[i];
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
