dd0356090062ef26a42bd7c3ee683495e5326a78
Osita <101451314+oceeta@users.noreply.github.com>
2024-09-17 15:57:00 +0100

    Write function to repeat strings for n times
    - The function works as long as n >= 0
    - If n < 0, it just returns an empty string
    - Pass 4/7 test cases

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(originalString, numberOfReps) {
+    let repeatedString = '';
+    let loopCounter = 1;
+    while (loopCounter <= numberOfReps) {
+        repeatedString += originalString;
+    loopCounter++;
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
