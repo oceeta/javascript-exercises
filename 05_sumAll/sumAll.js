fa92adde2a7e80fe9f6ee9b277f12feda2c0fd29
Osita <101451314+oceeta@users.noreply.github.com>
2024-09-18 22:27:10 +0100

    Add function to add positive integers in a range
    - Pass 7/7 unit tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum, secondNum) {
+    let result = 0;
+    if (!(Number.isInteger(firstNum) && Number.isInteger(secondNum)) ||
+        firstNum < 0 || secondNum < 0) {
+            return 'ERROR';
+    } else {
+        if (firstNum > secondNum) {
+            for (let i = secondNum; i <= firstNum; i++) {
+                result = result + i;
+            }
+        } else {
+            for (let i = firstNum; i <= secondNum; i++) {
+                result = result + i;
+            }
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
