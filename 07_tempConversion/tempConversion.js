b7a2da856b52353984ff9322ba43cf324c26bbeb
Osita <101451314+oceeta@users.noreply.github.com>
2024-09-19 09:51:16 +0100

    Add functions to convert between temperature units
    - Pass 6/6 unit tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFahrenheit) {
+  const tempCelsius = (tempFahrenheit - 32) * (5 / 9);
+  if (Number.isInteger(tempCelsius)) {
+    return tempCelsius;
+  } else {
+    return parseFloat(tempCelsius.toFixed(1));
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCelsius) {
+  const tempFahrenheit = (tempCelsius * 9 / 5 + 32);
+  if (Number.isInteger(tempFahrenheit)) {
+    return tempFahrenheit;
+  } else {
+    return parseFloat(tempFahrenheit.toFixed(1));
+  }
 };
 
 // Do not edit below this line
