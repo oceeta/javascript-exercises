205d5408e492daaf39863faaeef3f1150805be66
Osita Nwajah <101451314+oceeta@users.noreply.github.com>
2026-01-16 10:58:40 +0100

    Refactor tempConversion functions
    The earlier version of this code used the toFixed() method, which
    returned a string that had to then be converted back to a number using
    the parseFloat() function. This added an unecessary extra step in the
    functions' execution since the starting values were numbers to begin
    with.
    
    Another problem created by using the toFixed() method was that even
    integer results were always represented using fixed point notation. For
    example, a result of 0 would become 0.0 because of toFixed(1). This was
    what motivated the supposed need to check if the results were integers
    so they could be returned as is. It worked, but at the cost of making
    the code more verbose than it needed to be.
    
    Both of these problems were solved elegantly by using the Math.round()
    function to round the results to 1 decimal place without converting them
    to strings. This also meant that integers were returned as integers
    while floating point numbers were rounded to 1 decimal place, satisfying
    all test cases.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,19 +1,11 @@
 const convertToCelsius = function(tempFahrenheit) {
   const tempCelsius = (tempFahrenheit - 32) * (5 / 9);
-  if (Number.isInteger(tempCelsius)) {
-    return tempCelsius;
-  } else {
-    return parseFloat(tempCelsius.toFixed(1));
-  }
+  return Math.round(tempCelsius * 10)/10;
 };
 
 const convertToFahrenheit = function(tempCelsius) {
   const tempFahrenheit = (tempCelsius * 9 / 5 + 32);
-  if (Number.isInteger(tempFahrenheit)) {
-    return tempFahrenheit;
-  } else {
-    return parseFloat(tempFahrenheit.toFixed(1));
-  }
+  return Math.round(tempFahrenheit * 10)/10;
 };
 
 // Do not edit below this line
