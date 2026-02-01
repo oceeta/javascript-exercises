ee152bb36e8cb66ac665cd332d36c3c522e4a1bc
Osita Nwajah <101451314+oceeta@users.noreply.github.com>
2026-02-02 00:54:27 +0100

    Write multiply() function
     - Pass 10/16 tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -10,8 +10,8 @@ const sum = function(array) {
 	return array.reduce((sum, currentValue) => sum + currentValue, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((product, currentValue) => product * currentValue, 1);
 };
 
 const power = function() {
