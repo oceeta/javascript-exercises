02093b4e081baa47437dd25de0d6e91956eb4013
Osita Nwajah <101451314+oceeta@users.noreply.github.com>
2026-02-02 00:52:24 +0100

    Write sum function
     - Pass 8/16 tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,8 +6,8 @@ const subtract = function(a, b) {
 	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((sum, currentValue) => sum + currentValue, 0);
 };
 
 const multiply = function() {
