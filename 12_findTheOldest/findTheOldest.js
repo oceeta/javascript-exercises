c42db08cac1e84f44b163ad23f242f8649ff4451
Osita Nwajah <101451314+oceeta@users.noreply.github.com>
2026-02-03 00:07:05 +0100

    Write findTheOldest() function
     - Pass 3/3 tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    return array.reduce((oldest, currentPerson) => {
+        const currentYear = new Date().getFullYear();
+        if ("yearOfDeath" in currentPerson === false) {
+            if (currentYear - currentPerson.yearOfBirth > oldest.yearOfDeath - oldest.yearOfBirth) {
+                oldest = currentPerson;
+            } 
+        } else if ("yearOfDeath" in oldest === false) {
+            if (currentYear - oldest.yearOfBirth < currentPerson.yearOfDeath - currentPerson.yearOfBirth) {
+                oldest = currentPerson;
+            } 
+        }
+        if (currentPerson.yearOfDeath - currentPerson.yearOfBirth > oldest.yearOfDeath - oldest.yearOfBirth) {
+            oldest = currentPerson;
+        }
+        return oldest;
+    });
 };
 
 // Do not edit below this line
