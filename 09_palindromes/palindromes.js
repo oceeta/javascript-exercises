47111c6df2e66a82c543ec24642ebb21488597b0
Osita Nwajah <101451314+oceeta@users.noreply.github.com>
2026-02-02 02:40:48 +0100

    Write palindromes() function
     - Pass 8/8 tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let onlyAlphanumeric = string
+                            .split("")
+                            .filter((char) => char.match(/[A-Za-z0-9]/))
+                            .map((char) => char.toLowerCase());
 
+    const onlyAlphanumericReversed = onlyAlphanumeric.toReversed().join('');
+
+    onlyAlphanumeric = onlyAlphanumeric.join('');
+
+    return onlyAlphanumeric === onlyAlphanumericReversed
 };
 
 // Do not edit below this line
